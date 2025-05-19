14174786c46005fa909dc97056e4d473a3fe3d62
Satyam Raj <sstymrj@gmail.com>
2025-05-19 11:49:34 +0530

    Create Clousre-Function.js

---
+++ Clousre-Function.js
@@ -0,0 +1,46 @@
+/*
+Closure Function - It allows a fucntion to access the variable from its outer function even after it has finished their execution.
+----------------------------------------------
+Create a Calculator Functionality
+Cal(first,second)
+add() sub() mul () div()
+----------------------------------------------
+*/
+
+function calc(first,second){
+  var add = function(){
+    return first+second
+  }
+  var sub = function(){
+    return first-second
+  }
+  var mul = function(){
+    return first*second
+  }
+  var add = function(){
+    return first/second
+  }
+  return {add,sub,mul,div} //obj short handle style key-pair value type
+}
+
+var obj = calc(10,20); // this function returns the object so we will store it 
+
+//accessing method 1. using dot operator 
+
+obj.add(); 
+obj.sub();
+
+//accessing method 1. using Associates
+var input = 'add' // keep changing here only no need to change in code
+obj[input]();
+
+obj[input]();  //change input to 'sub' and so on....
+obj[input]();
+obj[input]();
+obj[input]();
+
+/*
+        Results 
+  1. Not able to directly access the variables 
+  2. Even our function is not directly accessible its only using the object.
+/*
