7424e022f078ba213b5294aa67a34b1ac03a57b2
Satyam Raj <sstymrj@gmail.com>
2025-05-24 13:24:40 +0530

    Create arrayMethods.js

---
+++ Practice_Questions/Basics/arrayMethods.js
@@ -0,0 +1,15 @@
+let months = ["january","july","march","august"];
+//july,june,march,august
+
+months.shift();
+'january'
+months
+(3) ['july', 'march', 'august']
+months.shift();
+'july'
+months.unshift("june");
+3
+months.unshift("july");
+4
+months;
+(4) ['july', 'june', 'march', 'august']
