eed1ee1f0a15ee3b178b2400902fa251e5b78b03
Satyam Raj <sstymrj@gmail.com>
2025-05-24 13:10:22 +0530

    Update StringMethods.js

--- Practice_Questions/Basics/StringMethods.js
+++ Practice_Questions/Basics/StringMethods.js
@@ -1,1 +1,22 @@
 
+let msg = "   Hi Satyam, How are you?           "
+
+console.log(msg.trim().toUpperCase());
+HI SATYAM, HOW ARE YOU?
+
+let pass = "  345@satyam!  "
+
+let password = pass.trim();
+
+password.slice(4,10)
+'satyam'
+
+password.indexOf("satyam")
+4
+
+password.replace("345","123");
+'123@satyam!'
+
+password = password.replace("345","123");
+'123@satyam!'
+﻿
